99807f5c169a3b9d8148c3ec2ccbdbd9b4eefed1
iamwxz <iamwxz@users.noreply.github.com>
2018-07-10 23:12:30 +0800

    2(c) OK

--- assignment2/q2_parser_transitions.py
+++ assignment2/q2_parser_transitions.py
@@ -21,6 +21,9 @@ class PartialParse(object):
         self.sentence = sentence
 
         ### YOUR CODE HERE
+        self.stack = ["ROOT"]
+        self.buffer = list(self.sentence)
+        self.dependencies = []
         ### END YOUR CODE
 
     def parse_step(self, transition):
@@ -32,6 +35,16 @@ class PartialParse(object):
                         transition.
         """
         ### YOUR CODE HERE
+        if transition == "S":
+            self.stack.append(self.buffer.pop(0))
+        elif transition == "LA":
+            self.dependencies.append((self.stack[-1], self.stack[-2]))
+            lastWord = self.stack.pop()
+            self.stack.pop()
+            self.stack.append(lastWord)
+        elif transition == "RA":
+            self.dependencies.append((self.stack[-2], self.stack[-1]))
+            self.stack.pop()
         ### END YOUR CODE
 
     def parse(self, transitions):
